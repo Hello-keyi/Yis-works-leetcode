d87dfe4b47374db0f24b273de96a4f88fc05af7e
yi <13692959211.com>
2026-06-20 20:06:03 +0800

    2026.6.20

--- 1.两数之和.py.py
+++ 1.两数之和.py.py
@@ -1,7 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dic = {}
-        
+#这里使用哈希表
+#这里学习新的函数方法 enumerate 这里第一个数字是索引，第二个数字是内容
+#由于 a + b = b + a 我们只需要一个就好，先把一份放在字典里面，后面再来检验
         for j , x in enumerate(nums):
             need = target - x
             if need in dic:
--- 217.存在重复元素.py.py
+++ 217.存在重复元素.py.py
@@ -1,6 +1,9 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         dic = {}
+#先弄一个字典，方便后续存储数据
+#这些题目的典型特点是，先用for 和enumerate遍历列表，然后用条件判断
+# 不符合就塞在字典里，符合就return
         for j,i  in enumerate(nums):
             if i in dic:
                 return True
